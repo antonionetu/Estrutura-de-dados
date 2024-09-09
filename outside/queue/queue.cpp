3bca569af321a18113952951ae13a4544695af0a
Antônio Fernandes <devantonio.fer@gmail.com>
2024-09-09 16:41:03 -0300

    adding queue

---
+++ outside/queue/queue.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+const int size = 10;
+int queue[size], last_index;
+
+
+void enqueue(int val){
+    if (last_index == size){
+        std::cout << "It is a QueueOverflow lol" << std::endl;
+        return;
+    }
+
+    queue[last_index] = val;
+    last_index++;
+}
+
+
+void dequeue(){
+    if (last_index == 0){
+        std::cout << "Queue is already empty" << std::endl;
+        return;
+    }
+
+    last_index--;
+
+    for(int c=0; c < last_index; c++){
+        queue[c] = queue[c+1];
+    }
+}
+
+
+int main(){
+    enqueue(4);
+    enqueue(2);
+    dequeue();
+    enqueue(24);
+
+    for(int c=0; c < size; c++){ std::cout << queue[c] << std::endl; }
+    return 0;
+}
+
